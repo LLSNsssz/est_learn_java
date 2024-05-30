c6a7d3c0cf5bd10d0dc87f4a8957b783a0b446f5
LLSNsssz <seungjang93@gmail.com>
2024-05-30 14:32:09 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
 public class Test {
 
   public static void main(String[] args) {
-    short sVal = 10;
-    short result = (short) (sVal + 10);
-    System.out.println(result);
+    float myFloat = 9.75f;
+
+    float anotherFloat = myFloat * 2;
+
+    System.out.println("myFloat = " + (int) myFloat);
+    System.out.println("anotherFloat = " + (byte) anotherFloat);
   }
 }
