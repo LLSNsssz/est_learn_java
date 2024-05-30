c35c29dad1717809111200168d09e2a4ea8e8e06
LLSNsssz <seungjang93@gmail.com>
2024-05-30 13:24:28 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -2,9 +2,16 @@
 public class Test {
 
     public static void main(String[] args) {
-        int a = 30;
-        int b = 40;
-        int c = 30;
-        System.out.println(a + b + c);
+        // int a = 1;
+        // 정수형 자료형
+        int a = 1;
+        byte b = 2;
+        long c = 3;
+        short d = 4;
+        //실수형 자료형
+        float e = 0.1f;
+        double f = 0.2;
+
+        char g = 'A';
     }
 }
