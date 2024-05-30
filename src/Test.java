9dcec2deb829bd87203d7b4cdd1ea237dd16cb1a
LLSNsssz <seungjang93@gmail.com>
2024-05-30 15:33:39 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Test {
 
   public static void main(String[] args) {
-    Galaxy galaxy = new Galaxy();
-    galaxy.call();
-    galaxy.name();
-
-    Iphone iphone = new Iphone();
-    iphone.call();
-    iphone.name();
+    List<Integer> list = Arrays.asList(1);
+    System.out.println(list.get(0));
   }
 }
