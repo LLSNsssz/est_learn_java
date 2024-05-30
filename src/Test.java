45135361727824c9aceeaad86915a98edc7e74cd
LLSNsssz <seungjang93@gmail.com>
2024-05-30 13:50:23 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,6 @@
-
 public class Test {
 
-    public static void main(String[] args) {
-        // int a = 1;
-        // 정수형 자료형
-        int a = 1;
-        byte b = 2;
-        long c = 3;
-        short d = 4;
-        //실수형 자료형
-        float e = 0.1f;
-        double f = 0.2;
-
-        char g = 'A';
-    }
+  public static void main(String[] args) {
+    int a = 1111122223333; //int 범위 넘음
+  }
 }
