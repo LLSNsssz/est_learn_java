7a9ea1531682d137603b397120651cc60cd7f353
LLSNsssz <seungjang93@gmail.com>
2024-05-30 09:34:33 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(i);
+    }
+
+
+}
