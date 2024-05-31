2ff6a747df252b34b22938d53fbc4b30647f0600
LLSNsssz <seungjang93@gmail.com>
2024-05-31 10:59:15 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 public class Test {
 
   public static void main(String[] args) {
-    String a = "WEEEE ASKE ;ND";
-    System.out.println(a.substring(0, 3) + a.substring(8, 9) + a.substring(12, 14));
+    String a = "a:b:c:d";
+
+    String[] split = a.split(":");
   }
 }
