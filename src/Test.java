365977f444940f69eedaf707531b413d0936a4a5
LLSNsssz <seungjang93@gmail.com>
2024-05-31 10:51:09 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,7 @@
 public class Test {
 
   public static void main(String[] args) {
-    String a =
-        "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! ";
-    System.out.println(a.replaceAll("World", "Java"));
-
-    System.out.println(a.replace("World", ""));
+    String a = "WEEEE ASKE ;ND";
+    System.out.println(a.substring(0, 3) + a.substring(8, 9) + a.substring(12, 14));
   }
 }
