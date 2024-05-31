6987e290efbd49af9bcd735debf9fd0cef2222dd
LLSNsssz <seungjang93@gmail.com>
2024-05-31 13:07:28 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,12 @@
 public class Test {
 
   public static void main(String[] args) {
-    String a = "HELLO WORLD";
-    String concat = a.concat("!");
-    System.out.println(concat);
+    String name1 = "Tim";
+    int age1 = 30;
+    String name2 = "Anna";
+    int age2 = 45;
+
+    System.out.println(String.format("%s의 나이는 %s세 입니다", name1, age1));
+    System.out.println(String.format("%s의 나이는 %s세 입니다", name2, age2));
   }
 }
