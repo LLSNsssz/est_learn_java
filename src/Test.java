16cde0d6993f35dab4f6e610ee30aa706060aae5
LLSNsssz <seungjang93@gmail.com>
2024-05-31 13:35:36 +0900

    init

--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,14 @@
 public class Test {
 
   public static void main(String[] args) {
-    String result = "";
 
-    result += "Hello ";
-    result += "java ";
-    result += "World";
-
-    System.out.println(result);
-
-    StringBuilder sb = new StringBuilder();
+    StringBuffer sb = new StringBuffer();
     sb.append("Hello ");
-    sb.append("java ");
-    sb.append("World");
-    System.out.println(sb); // sout이 toString()을 알아서 호출한다.
-
-    StringBuffer sf = new StringBuffer();
-    sf.append("Hello ");
-    sf.append("java ");
-    sf.append("World");
-    System.out.println(sf);
+    sb.insert(0, "World");
+    sb.insert(0, "World");
+    sb.insert(0, "World");
+    sb.insert(0, "World");
+    sb.insert(0, "World");
+    System.out.println(sb);
   }
 }
