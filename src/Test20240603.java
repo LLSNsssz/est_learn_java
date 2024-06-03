377e01ae570e86fc767bdd4fca61ad94ad11b9de
LLSNsssz <seungjang93@gmail.com>
2024-06-03 14:11:53 +0900

    init

--- src/Test20240603.java
+++ src/Test20240603.java
@@ -1,33 +1,10 @@
 public class Test20240603 {
   public static void main(String[] args) {
-    int x = 10;
-
-    System.out.println(x > 0 ? "양수" : x < 0 ? "양수" : 0);
-
-    int score = 75;
-
-    if (score >= 90) {
-      System.out.println("A");
-    } else if (score >= 80) {
-      System.out.println("B");
-    } else if (score >= 70) {
-      System.out.println("C");
-    } else if (score >= 60) {
-      System.out.println("D");
-    } else {
-      System.out.println("F");
-    }
-
-    int year = 2023;
-    System.out.println(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? "윤년" : "평년");
-
     int a = 10;
-    int b = 20;
-    int c = 30;
-    System.out.println(a > b ? a : b > c ? b : c);
+    int b = 5;
 
-    int passScore = 60;
-    int myScore = 75;
-    System.out.println(passScore > myScore ? "불합격" : "합격");
+    boolean result1 = (a > 5) && (b < 10);
+    boolean result2 = (a > 5) || (b > 10);
+    boolean result3 = !(a > 5);
   }
 }
