367c61791e9be8f27f1a70525ce7c2c40e188d3f
LLSNsssz <seungjang93@gmail.com>
2024-06-03 09:24:42 +0900

    init

---
+++ src/Test20240603.java
@@ -0,0 +1,22 @@
+public class Test20240603 {
+  public static void main(String[] args) {
+    int number1 = 10;
+    int number2 = 3;
+
+    int sum = number1 + number2;
+    int diff1 = number2 - number1;
+    int diff2 = number1 - number2;
+
+    System.out.println("sum = " + sum);
+    System.out.println("diff1 = " + diff1);
+    System.out.println("diff2 = " + diff2);
+
+    int product = number1 + number2;
+    int quot = number1 / number2;
+    int remainder = number1 % number2;
+
+    System.out.println("product = " + product);
+    System.out.println("quot = " + quot);
+    System.out.println("remainder = " + remainder);
+  }
+}
