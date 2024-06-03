7127567075fbcd76b6e5ba13b5e05e0f1b52163d
LLSNsssz <seungjang93@gmail.com>
2024-06-03 13:02:47 +0900

    init

--- src/Test20240603.java
+++ src/Test20240603.java
@@ -1,14 +1,12 @@
 public class Test20240603 {
   public static void main(String[] args) {
+    int score = 95;
+    //    String grade = (score > 90) && (score % 5 == 0) ? "Good" : "Bad";
 
-    System.out.println(1 ^ ~0);
-    System.out.println(1 ^ ~1);
-    System.out.println(0 ^ ~0);
-
-    System.out.println(0b1111001011 & 0b1111);
-    System.out.println(0b11110 | 0b1110);
-
-    System.out.println(0b1111);
-    System.out.println(0b1111001011);
+    if ((score > 90) && (score % 5 == 0)) {
+      System.out.println("good");
+    } else {
+      System.out.println("bad");
+    }
   }
 }
