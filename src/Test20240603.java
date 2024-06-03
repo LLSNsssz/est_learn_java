b23bd54b1ecc9a8819a05153902cec2710159bf2
LLSNsssz <seungjang93@gmail.com>
2024-06-03 13:20:40 +0900

    init

--- src/Test20240603.java
+++ src/Test20240603.java
@@ -1,16 +1,17 @@
 public class Test20240603 {
   public static void main(String[] args) {
-    int number = 10;
+    int a = 10;
+    int b = 10;
 
-    if (number > 10) {
-      System.out.println("number는 10보다 큽니다.");
-    } else if (number < 10) {
-      System.out.println("number는 10보다 작습니다.");
-      if (number > 5) {
-        System.out.println("number는 5보다 큽니다.");
-      }
+    if (a > b) {
+      System.out.println("a is greater than b.");
+    } else if (a < b) {
+      System.out.println("a is less than b.");
     } else {
-      System.out.println("number는 10입니다.");
+      System.out.println("a and b are equal.");
     }
+
+    System.out.println(
+        a > b ? "a is greater than b." : a < b ? "a is less than b." : "a and b are equal.");
   }
 }
