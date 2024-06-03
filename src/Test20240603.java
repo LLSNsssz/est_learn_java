f0e19748dee72968a17d185ebe11243e086d422c
LLSNsssz <seungjang93@gmail.com>
2024-06-03 11:17:06 +0900

    init

--- src/Test20240603.java
+++ src/Test20240603.java
@@ -1,8 +1,14 @@
 public class Test20240603 {
   public static void main(String[] args) {
-    int x = 3195;
-    System.out.println(Integer.toBinaryString(x));
-    System.out.println(Integer.toOctalString(x));
-    System.out.println(Integer.toHexString(x));
+
+    System.out.println(1 ^ ~0);
+    System.out.println(1 ^ ~1);
+    System.out.println(0 ^ ~0);
+
+    System.out.println(0b1111001011 & 0b1111);
+    System.out.println(0b11110 | 0b1110);
+
+    System.out.println(0b1111);
+    System.out.println(0b1111001011);
   }
 }
