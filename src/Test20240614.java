7e34f6f57a2b3344c6abc8aa3cfc5c958bc82b80
LLSNsssz <seungjang93@gmail.com>
2024-06-04 11:21:15 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -2,16 +2,14 @@ import java.util.stream.IntStream;
 
 public class Test20240614 {
   public static void main(String[] args) {
-    int[] number = {4, -2, 9, -7, 5, 1, -3, 6, -1, 8};
-    int i = 0, sumNegative = 0, sumPositive = 0;
-    while (i < number.length) {
-      if (number[i] > 0) {
-        sumPositive += number[i++];
-      } else if (number[i] < 0) {
-        sumNegative += number[i++];
-      }
+
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+      if (i % 2 == 0) sum += i;
     }
-    System.out.println("sumNegative = " + sumNegative);
-    System.out.println("sumPositive = " + sumPositive);
+    System.out.println("sum = " + sum);
+
+    sum = IntStream.range(0, 101).filter(i -> i % 2 == 0).sum();
+    System.out.println("sum = " + sum);
   }
 }
