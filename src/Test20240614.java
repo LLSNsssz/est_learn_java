7b403d6c714ec8b71433766a1a769c25fdd13dfd
LLSNsssz <seungjang93@gmail.com>
2024-06-04 15:34:33 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -5,9 +5,13 @@ import java.util.stream.Stream;
 public class Test20240614 {
   public static void main(String[] args) {
 
-    for (int i = 0; i < 11; i++) {
-      if (i == 4 || i == 7) continue;
-      System.out.println(i);
+    int[] scores = {1, 30, 20, 80, 15, 20, 30, 22};
+    int sum = 0;
+    for (int score : scores) {
+      sum += score;
     }
+    double average = (double) sum / scores.length;
+    System.out.println("sum = " + sum);
+    System.out.println("average = " + average);
   }
 }
