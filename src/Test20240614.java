328c7b03bd46d43b7c9eac32434e6238fd3fd7b4
LLSNsssz <seungjang93@gmail.com>
2024-06-04 11:14:57 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -2,18 +2,16 @@ import java.util.stream.IntStream;
 
 public class Test20240614 {
   public static void main(String[] args) {
-    int[] number = {10, 5, 8, 20, 3, 15, 9, 2};
-    int i = 0, j = 0, max = 0, min = Integer.MAX_VALUE;
+    int[] number = {4, -2, 9, -7, 5, 1, -3, 6, -1, 8};
+    int i = 0, sumNegative = 0, sumPositive = 0;
     while (i < number.length) {
-      max = number[i] > max ? number[i] : max;
-      i++;
+      if (number[i] > 0) {
+        sumPositive += number[i++];
+      } else if (number[i] < 0) {
+        sumNegative += number[i++];
+      }
     }
-    System.out.println(max);
-
-    while (j < number.length) {
-      min = number[j] < min ? number[j] : min;
-      j++;
-    }
-    System.out.println(min);
+    System.out.println("sumNegative = " + sumNegative);
+    System.out.println("sumPositive = " + sumPositive);
   }
 }
