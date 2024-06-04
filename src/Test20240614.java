d0f37024869b5b846aa45634c3f8ddfe8b009d62
LLSNsssz <seungjang93@gmail.com>
2024-06-04 10:24:15 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -2,10 +2,18 @@ import java.util.stream.IntStream;
 
 public class Test20240614 {
   public static void main(String[] args) {
-    int i = 0;
-    while (i < 300) {
-      i += 3;
-      System.out.println(i);
+    int[] number = {10, 5, 8, 20, 3, 15, 9, 2};
+    int i = 0, j = 0, max = 0, min = Integer.MAX_VALUE;
+    while (i < number.length) {
+      max = number[i] > max ? number[i] : max;
+      i++;
     }
+    System.out.println(max);
+
+    while (j < number.length) {
+      min = number[j] < min ? number[j] : min;
+      j++;
+    }
+    System.out.println(min);
   }
 }
