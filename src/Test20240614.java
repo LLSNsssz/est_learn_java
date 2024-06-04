3fc97e228d588f885c44ff79a6d719e031c143c6
LLSNsssz <seungjang93@gmail.com>
2024-06-04 15:24:51 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Test20240614 {
   public static void main(String[] args) {
-    for (int i = 19; i >= 2; i--) {
-      for (int j = 19; j >= 1; j--) {
-        System.out.println(i + "x" + j + "=" + i * j);
-      }
+
+    for (int i = 0; i < 11; i++) {
+      if (i == 4 || i == 7) continue;
+      System.out.println(i);
     }
   }
 }
