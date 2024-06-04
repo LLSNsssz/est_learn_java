cdcaf65f4f7aef2f377780bac4ededb3c02a579e
LLSNsssz <seungjang93@gmail.com>
2024-06-04 11:31:24 +0900

    init

--- src/Test20240614.java
+++ src/Test20240614.java
@@ -3,13 +3,14 @@ import java.util.stream.IntStream;
 public class Test20240614 {
   public static void main(String[] args) {
 
-    int sum = 0;
-    for (int i = 1; i <= 100; i++) {
-      if (i % 2 == 0) sum += i;
+    int sum = 0, n = 20;
+    for (int i = 0; i < n; i++) {
+      if (i % 15 == 0) {
+        sum += i;
+      }
     }
-    System.out.println("sum = " + sum);
-
-    sum = IntStream.range(0, 101).filter(i -> i % 2 == 0).sum();
-    System.out.println("sum = " + sum);
+    System.out.println(sum);
+    sum = IntStream.rangeClosed(1, n).filter(i -> i % 15 == 0).sum();
+    System.out.println(sum);
   }
 }
