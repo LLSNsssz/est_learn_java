3028299d8cb5fe5f6fe102db7cf41a2c16581a52
LLSNsssz <seungjang93@gmail.com>
2024-06-10 15:15:53 +0900

    init

--- src/Test20240610.java
+++ src/Test20240610.java
@@ -1,32 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-
 public class Test20240610 {
 
   public static void main(String[] args) {
-    //    3개의 정수 인자 a, b, c가 주어졌을 때, 이들의 합을 반환하세요.
-    //    하지만, 인자 중 하나가 다른 인자와 동일하면, 그 숫자는 합산에서 제외합니다.
-    //    기본적으로 중복되지 않는 숫자만 합산합니다.
+    // 주어진 문자열에서 각 문자마다 동일한 문자를 한 번 더 추가한 문자열을 반환하세요.
 
-    System.out.println(sumUnique(1, 2, 3)); // 6
-    System.out.println(sumUnique(3, 2, 3)); // 2
-    System.out.println(sumUnique(3, 3, 3)); // 0
+    System.out.println(repeatChar("The")); // "TThhee"
+    System.out.println(repeatChar("AAbb")); // "AAAAbbbb"
+    System.out.println(repeatChar("Hi-There")); // "HHii--TThheerree"
   }
 
-  public static int sumUnique(int a, int b, int c) {
-    //    HashSet<Integer> set = new HashSet<>(Arrays.asList(a, b, c));
-    //    ArrayList<Integer> list = new ArrayList<>(set);
-    //    Collections.sort(list);
-    //    if (set.size() == 3) return a + b + c;
-    //    else if (set.size() == 2) return list.get(0);
-    //    else return 0;
-    if (a == b && b == c) return 0;
-    if (a == b) return c;
-    if (a == c) return b;
-    if (b == c) return a;
-    return a + b + c;
+  public static String repeatChar(String str) {
+    String result = "";
+
+    for (int i = 0; i < str.length(); i++) {
+      result += "" + str.charAt(i) + str.charAt(i);
+    }
+    return result;
   }
 }
