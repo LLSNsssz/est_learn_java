6509ee50341aed2e959e51b6a1913f4d237a45d6
LLSNsssz <seungjang93@gmail.com>
2024-06-10 18:30:09 +0900

    init

--- src/Test20240610.java
+++ src/Test20240610.java
@@ -1,19 +1,18 @@
 public class Test20240610 {
 
   public static void main(String[] args) {
-    // 주어진 문자열에서 각 문자마다 동일한 문자를 한 번 더 추가한 문자열을 반환하세요.
+    // 주어진 문자열에서 모든 별표(*)와 별표 바로 왼쪽 및 오른쪽에 있는 문자를 제거한 버전을 반환하세요.
+    // 예를 들어, "abcd"는 "ad"를 반환하고, "ab**cd"도 "ad"를 반환합니다.
 
-    System.out.println(repeatChar("The")); // "TThhee"
-    System.out.println(repeatChar("AAbb")); // "AAAAbbbb"
-    System.out.println(repeatChar("Hi-There")); // "HHii--TThheerree"
+    System.out.println(starSideDel("cd*zq")); // "cq"
+    System.out.println(starSideDel("ab****cd")); // "ad"
+    System.out.println(starSideDel("wacy***xko")); // "wacko"
   }
 
-  public static String repeatChar(String str) {
-    String result = "";
+  public static String starSideDel(String str) {
 
-    for (int i = 0; i < str.length(); i++) {
-      result += "" + str.charAt(i) + str.charAt(i);
-    }
-    return result;
+    String string =
+        str.substring(0, str.indexOf("*") - 1) + str.substring(str.lastIndexOf("*") + 2);
+    return string;
   }
 }
