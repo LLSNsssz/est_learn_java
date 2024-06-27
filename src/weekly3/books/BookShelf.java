2681cd02196d79e3eb3fb2140ea7c981f1a68132
LLSNsssz <seungjang93@gmail.com>
2024-06-27 22:35:53 +0900

    Update weekly3

--- src/weekly3/books/BookShelf.java
+++ src/weekly3/books/BookShelf.java
@@ -33,25 +33,21 @@ public class BookShelf<T> implements BookManager<T> {
   /** books 리스트에서 매개변수 title 을 포함하는 도서 리스트를 반환 하는 메서드 */
   @Override
   public List<Book<T>> searchByTitle(String title) {
+    return searchBook(title);
+  }
+
+  /** books 리스트에서 매개변수 author 와 일치하는 도서 리스트를 반환 하는 메서드 */
+  @Override
+  public List<Book<T>> searchByAuthor(String author) {
+    return searchBook(author);
+  }
+
+  /** 입력받은 문자열이 title 이나 author 에 포함되어있는 책 리스트 반환 */
+  private List<Book<T>> searchBook(String s) {
     List<Book<T>> resultList = new ArrayList<>();
     for (int i = 0; i < books.size(); i++) {
       String bookTitle = books.get(i).getTitle();
-      if (!bookTitle.contains(title)) {
-        continue;
-      } else {
-        resultList.add(books.get(i));
-      }
-    }
-    return resultList;
-  }
-
-  /** books 리스트에서 매개변수 author와 일치하는 도서 리스트를 반환 하는 메서드 */
-  @Override
-  public List<Book<T>> searchByAuthor(String author) {
-    List<Book<T>> resultList = new ArrayList<>();
-    for (int i = 0; i < books.size(); i++) {
-      String bookAuthor = books.get(i).getAuthor();
-      if (!bookAuthor.equals(author)) {
+      if (!bookTitle.contains(s)) {
         continue;
       } else {
         resultList.add(books.get(i));
